be411a8ff972e7f877cb5efa6bd01e2003c1f90f
dev104 <90598001+dev104@users.noreply.github.com>
2024-05-02 06:02:34 +0530

    Add files via upload

---
+++ homeAssignments/week1/day1/PrintOddNumbers.java
@@ -0,0 +1,25 @@
+package homeAssignments.week1.day1;
+
+public class PrintOddNumbers {
+
+		public static void main (String args[]) 
+		{
+			
+			int s = 10;
+			
+			System.out.println("The Odd Numbers are: ");
+			
+			for (int i=0; i<=s; i++) {
+				
+				if(i%2 != 0) {
+					System.out.println(i);
+					
+				}
+			}
+			
+			
+		}
+
+	}
+
+
